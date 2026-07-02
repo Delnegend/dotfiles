b3a60bb732b0c2ccf50c06726dfaf62057b6ed13
Delnegend <kiennguyen19323@gmail.com>
2026-07-02 11:39:05 +0700

    chore(verso.sh): drop redundant Homebrew env vars
    - Removed `HOMEBREW_*`, `PATH`, `MANPATH`, and `INFOPATH`
      exports duplicated from `brew shellenv` in `00-env.sh`

--- .bashrc.d/00-env.sh
+++ .bashrc.d/00-env.sh
@@ -1,1 +1,4 @@
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+echo >> /home/$USER/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
--- .bashrc.d/machines/verso.sh
+++ .bashrc.d/machines/verso.sh
@@ -1,8 +1,2 @@
 alias k3s-check='sudo CONFIG=/usr/lib/modules/$(uname -r)/config k3s check-config'
 alias sync='just sync'
-export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
-export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
-export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
-export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}"
-[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
-export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
