bb6a0b2c0bda76dad8672f5d9ed3b458102481a0
Delnegend <kiennguyen19323@gmail.com>
2026-07-02 11:52:19 +0700

    refactor(bashrc): move bazzite-specific env vars from shared `00-env.sh` to `bazzite.sh`
    - Removed `SSH_AUTH_SOCK`, `XMODIFIERS`, and `PATH` (jdk) from `.bashrc.d/00-env.sh`
    - Added them to `.bashrc.d/machines/bazzite.sh` so they only apply on the Bazzite machine

--- .bashrc.d/00-env.sh
+++ .bashrc.d/00-env.sh
@@ -1,4 +1,2 @@
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-export SSH_AUTH_SOCK=/home/lava/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
-export XMODIFIERS=@im=fcitx
-export PATH=$PATH:/var/home/lava/bin/jdk-25.0.2+10-jre/bin/
+
--- .bashrc.d/machines/bazzite.sh
+++ .bashrc.d/machines/bazzite.sh
@@ -1,3 +1,7 @@
 if [ -f /usr/share/bazzite-cli/bling.sh ]; then
     source /usr/share/bazzite-cli/bling.sh
 fi
+
+export SSH_AUTH_SOCK=/home/lava/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+export XMODIFIERS=@im=fcitx
+export PATH=$PATH:/var/home/lava/bin/jdk-25.0.2+10-jre/bin/
