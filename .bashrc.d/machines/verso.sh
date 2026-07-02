ac65f794f79f318eb954843abd86244d53ddbcc6
Delnegend <kiennguyen19323@gmail.com>
2026-07-02 10:27:46 +0700

    feat: add verso machine config with Homebrew env and k3s/sync aliases

---
+++ .bashrc.d/machines/verso.sh
@@ -0,0 +1,8 @@
+alias k3s-check='sudo CONFIG=/usr/lib/modules/$(uname -r)/config k3s check-config'
+alias sync='just sync'
+eval export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
+export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
+export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}"
+[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
