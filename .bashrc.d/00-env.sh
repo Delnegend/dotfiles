d1917c8e58e9f2496a9e7e91af1b5b39a8cea47a
Delnegend <kiennguyen19323@gmail.com>
2026-07-03 09:31:22 +0700

    chore(shell): add opencode bin dir to PATH
    - Add /home/biolak/.opencode/bin to the beginning of PATH in 00-env.sh
    - Ensures opencode CLI is available in every new shell session

--- .bashrc.d/00-env.sh
+++ .bashrc.d/00-env.sh
@@ -1,4 +1,5 @@
 export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+export PATH=/home/biolak/.opencode/bin:$PATH
 echo >> "/home/$(whoami)/.profile"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "/home/$(whoami)/.profile"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
